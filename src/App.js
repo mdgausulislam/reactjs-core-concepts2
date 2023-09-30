3628075c2b5f345220e0e3b90fea32fea8a898e9
unknown <gausulislam5@gmail.com>
2023-09-30 22:29:47 +0600

    update react components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    </div>
+  );
+}
+// function App() {
+//   const products=[
+//     {name:'laptop',price:75000},
+//     {name:'Mobile',price:10000},
+//     {name:'Watch',price:3000},
+//     {name:'tablet',price:30000}
+//   ]
+//   return (
+//     <div className="App">
+//       {
+//        products.map(product=><Product name={product.name} price={product.price}></Product>)
+//       }
+//     </div>
+//   );
+// }
+
+function Product(props){
+  return(
+    <div className="product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+
     </div>
   );
 }
